7a4eef0f45ebb521844008fdd94069b71e50243c
ARUNKUMARP123 <arun841988@gmail.com>
2024-05-17 16:06:48 +0530

    correction for env

--- db.js
+++ db.js
@@ -1,17 +1,15 @@
 const mongoose=require("mongoose");
 mongoose.set("strictQuery",false);
 
-
-
-const mongodb_URL=process.env.NODE_URI==="production"?process.env.MONGODB_URI:"mongodb://localhost:27017/GCES";
 const connectdb=async(res)=>{
+    const mongodb_URL=process.env.NODE_ENV==="production"?process.env.MONGODB_URL:"mongodb://localhost:27017/GCES";
     try {
         await mongoose.connect(mongodb_URL);
         if(mongoose.connection.readyState===2){
             res &&  res.send("Connecting......!")
         }
         if(mongoose.connection.readyState===1){
-            res&& res.send("Connected.")
+            res && res.send("Connected.")
         }
     } catch (error) {
         console.log(error,"connection failed");
