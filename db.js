be04e44c98d0856e3468ca6d9d64539e2309fc16
ARUNKUMARP123 <arun841988@gmail.com>
2025-05-22 10:30:04 +0530

    connection change

--- db.js
+++ db.js
@@ -1,22 +1,23 @@
-const mongoose=require("mongoose");
-mongoose.set("strictQuery",false);
-
-const connectdb=async(res)=>{
-    const mongodb_URL=process.env.NODE_ENV==="production"?process.env.MONGODB_URL:"mongodb://localhost:27017/GCES";
-    try {
-        await mongoose.connect(mongodb_URL);
-        if(mongoose.connection.readyState===2){
-            res &&  res.send("Connecting......!")
-        }
-        if(mongoose.connection.readyState===1){
-            res && res.send("Connected.")
-        }
-    } catch (error) {
-        console.log(error,"connection failed");
-       res && res.send("Dis-Connected.")
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const connectdb = async (res) => {
+  const mongodb_URL =
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGODB_URL
+      : "mongodb://localhost:27017/GCES";
+      
+  try {
+    await mongoose.connect(mongodb_URL);
+    if (mongoose.connection.readyState === 2) {
+      res && res.send("Connecting......!");
     }
-   
-}
+    if (mongoose.connection.readyState === 1) {
+      res && res.send("Connected.");
+    }
+  } catch (error) {
+    console.log(error, "connection failed");
+    res && res.send("Dis-Connected.");
+  }
+};
 
-
-module.exports={connectdb,mongoose,}
+module.exports = { connectdb, mongoose };
